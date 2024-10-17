605c43db81715421c556c6b131e59d2d255e0548
Kyle Kerr <kkerr2550@gmail.com>
2024-10-17 09:52:28 -0600

    fixed stray 'X's

--- week2/cool_pics/coolpics.js
+++ week2/cool_pics/coolpics.js
@@ -15,37 +15,37 @@ function handleResize() {
     }
 }
 
-function viewerTemplate(pic, alt) {
-    return  `<div class="viewer>
-    <span class="close-viewer>X</span>
-    </div>`;
-}
+// function viewerTemplate(pic, alt) {
+//     return  `<div class="viewer>
+//     <span class="close-viewer>X</span>
+//     </div>`;
+// }
 
-function closeViewer() {
-    document.querySelector(".viewer")?.remove();
-}
+// function closeViewer() {
+//     document.querySelector(".viewer")?.remove();
+// }
 
-handleResize();
+// handleResize();
 
-function viewHandler(event) {
-    const target = event.target;
-    console.dir(target);
-    if (target.tagName != "IMG") {
-        return;
-    }
-    const imgSrc = event.target.src.split("-");
-    const newSrc = imgSrc[0] + "-full.jpeg";
-    document.body.insertAdjacentHTML(
-        "afterbegin",
-        viewerTemplate(newSrc, target.alt)
-    );
+// function viewHandler(event) {
+//     const target = event.target;
+//     console.dir(target);
+//     if (target.tagName != "IMG") {
+//         return;
+//     }
+//     const imgSrc = event.target.src.split("-");
+//     const newSrc = imgSrc[0] + "-full.jpeg";
+//     document.body.insertAdjacentHTML(
+//         "afterbegin",
+//         viewerTemplate(newSrc, target.alt)
+//     );
 
-    document
-        .querySelector(".closeviewer")
-        .addEventListener("click", closeViewer);
-}
+//     document
+//         .querySelector(".closeviewer")
+//         .addEventListener("click", closeViewer);
+// }
 
 
 menuButton.addEventListener("click", toggleMenu);
-gallery.addEventListener("click", viewHandler);
+// gallery.addEventListener("click", viewHandler);
 window.addEventListener("resize", handleResize);
