a93f418339f3edb9ad83381f1794beaecae6ab77
Kyle Kerr <kkerr2550@gmail.com>
2024-10-29 22:07:40 -0600

    undid some sneaky camelcasing

--- final/items.mjs
+++ final/items.mjs
@@ -2,7 +2,7 @@ export const items = [
     {
         id: 1,
         itemName: "Bob-omb",
-        itemImage: "Skyblock_items_bob_omb.png",
+        itemImage: "SkyBlock_items_bob_omb.png",
         itemCount: 0,
         itemMultiplier: 20,
         itemWeight: 10000
@@ -10,7 +10,7 @@ export const items = [
     {
         id: 2,
         itemName: "Oil Barrel",
-        itemImage: "Skyblock_items_oil_barrel.png",
+        itemImage: "SkyBlock_items_oil_barrel.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 8000
@@ -18,7 +18,7 @@ export const items = [
     {
         id: 3,
         itemName: "Prehistoric Egg",
-        itemImage: "Skyblock_items_prehistoric_egg.png",
+        itemImage: "SkyBlock_items_prehistoric_egg.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 5000
@@ -26,7 +26,7 @@ export const items = [
     {
         id: 4,
         itemName: "Treasurite",
-        itemImage: "Skyblock_items_treasurite.png",
+        itemImage: "SkyBlock_items_treasurite.png",
         itemCount: 0,
         itemMultiplier: 5,
         itemWeight: 2000
@@ -34,7 +34,7 @@ export const items = [
     {
         id: 5,
         itemName: "Wishing Compass",
-        itemImage: "Skyblock_items_wishing_compass.png",
+        itemImage: "SkyBlock_items_wishing_compass.png",
         itemCount: 0,
         itemMultiplier: 3,
         itemWeight: 500
@@ -42,7 +42,7 @@ export const items = [
     {
         id: 6,
         itemName: "Diamond Essence",
-        itemImage: "Skyblock_items_diamond_essence.png",
+        itemImage: "SkyBlock_items_diamond_essence.png",
         itemCount: 0,
         itemMultiplier: 10,
         itemWeight: 750
@@ -50,7 +50,7 @@ export const items = [
     {
         id: 7,
         itemName: "Gold Essence",
-        itemImage: "Skyblock_items_gold_essence.png",
+        itemImage: "SkyBlock_items_gold_essence.png",
         itemCount: 0,
         itemMultiplier: 10,
         itemWeight: 750
@@ -58,7 +58,7 @@ export const items = [
     {
         id: 8,
         itemName: "Divan Fragment",
-        itemImage: "Skyblock_items_divan_fragment.png",
+        itemImage: "SkyBlock_items_divan_fragment.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 6000
@@ -66,7 +66,7 @@ export const items = [
     {
         id: 9,
         itemName: "Dwarven O's Gemstone Grahams",
-        itemImage: "Skyblock_items_dwarven_os_gemstone_grahams.png",
+        itemImage: "SkyBlock_items_dwarven_os_gemstone_grahams.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 1450
@@ -74,7 +74,7 @@ export const items = [
     {
         id: 10,
         itemName: "Lapidary I",
-        itemImage: "Skyblock_items_enchanted_book.gif",
+        itemImage: "SkyBlock_items_enchanted_book.gif",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 4000
@@ -82,7 +82,7 @@ export const items = [
     {
         id: 11,
         itemName: "Fortune IV",
-        itemImage: "Skyblock_items_enchanted_book.gif",
+        itemImage: "SkyBlock_items_enchanted_book.gif",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 700
@@ -90,7 +90,7 @@ export const items = [
     {
         id: 12,
         itemName: "Pickonimbus 2000",
-        itemImage: "Skyblock_items_enchanted_diamond_pickaxe.gif",
+        itemImage: "SkyBlock_items_enchanted_diamond_pickaxe.gif",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 10000
@@ -98,7 +98,7 @@ export const items = [
     {
         id: 13,
         itemName: "Gemstone Mixture",
-        itemImage: "Skyblock_items_gemstone_mixture.png",
+        itemImage: "SkyBlock_items_gemstone_mixture.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 700
@@ -106,7 +106,7 @@ export const items = [
     {
         id: 14,
         itemName: "Helix Fossil",
-        itemImage: "Skyblock_items_helix.png",
+        itemImage: "SkyBlock_items_helix.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 4000
@@ -114,7 +114,7 @@ export const items = [
     {
         id: 15,
         itemName: "Claw Fossil",
-        itemImage: "Skyblock_items_claw_fossil.png",
+        itemImage: "SkyBlock_items_claw_fossil.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 500
@@ -122,7 +122,7 @@ export const items = [
     {
         id: 16,
         itemName: "Jaderald",
-        itemImage: "Skyblock_items_jaderald.png",
+        itemImage: "SkyBlock_items_jaderald.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 1450
@@ -130,7 +130,7 @@ export const items = [
     {
         id: 17,
         itemName: "Jasper Crystal",
-        itemImage: "Skyblock_items_jasper_crystal.png",
+        itemImage: "SkyBlock_items_jasper_crystal.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 2500
@@ -138,7 +138,7 @@ export const items = [
     {
         id: 18,
         itemName: "Ruby Crystal",
-        itemImage: "Skyblock_items_ruby_crystal.png",
+        itemImage: "SkyBlock_items_ruby_crystal.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 2500
@@ -146,7 +146,7 @@ export const items = [
     {
         id: 19,
         itemName: "Precious Pearl",
-        itemImage: "Skyblock_items_precious_pearl.png",
+        itemImage: "SkyBlock_items_precious_pearl.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 500
@@ -154,7 +154,7 @@ export const items = [
     {
         id: 20,
         itemName: "Recall Potion",
-        itemImage: "Skyblock_items_recall_potion.png",
+        itemImage: "SkyBlock_items_recall_potion.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 250
@@ -162,7 +162,7 @@ export const items = [
     {
         id: 21,
         itemName: "Quick Claw",
-        itemImage: "Skyblock_items_pet_item_quick_claw.png",
+        itemImage: "SkyBlock_items_pet_item_quick_claw.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 50
@@ -170,7 +170,7 @@ export const items = [
     {
         id: 22,
         itemName: "Divan's Alloy",
-        itemImage: "Skyblock_items_divan_alloy.png",
+        itemImage: "SkyBlock_items_divan_alloy.png",
         itemCount: 0,
         itemMultiplier: 1,
         itemWeight: 17
